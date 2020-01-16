a0ccec6e097b6a32b77bd6c7b1c7a4439288d606
recon88 <info@ingoguther.de>
2020-01-16 21:53:38 +0100

    thread fix

--- spielemitanderen/recruiting.js
+++ spielemitanderen/recruiting.js
@@ -8,3 +8,47 @@ document.head.appendChild(script2);
 
 //document.getElementById('discussions').innerHTML = '';
 //document.getElementById('discussions').innerHTML = '<div class="discussions box main glow empty"><span class="glow">&nbsp;</span><h2><em>Test</em></h2></div>';
+
+if ($("#discussion").data().discussionId === "c1XylYOm") {
+	 <script src="https://cdn.jsdelivr.net/gh/recon88/boardsstuff/spielemitanderen/lazyload.min.js"></script>
+   <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/recon88/boardsstuff/spielemitanderen/freewall.min.js"></script>
+   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/recon88/boardsstuff/spielemitanderen/recap.min.css">
+   <script type="text/javascript">
+      $(function() {
+      	var myLazyLoad = new LazyLoad();
+      	$("html body .backdrop .header").attr("style","background:url(https://lolstatic-a.akamaihd.net/apollo/assets/custom/ezreal_2019.png)!important;background-repeat:no-repeat!important");
+      });
+   </script>
+   
+    <script type="text/javascript">
+      $(function() {
+      	var wall = new Freewall("#freewall");
+      	wall.reset({
+      		selector: '.brick',
+      		animate: true,
+      		cellW: 160,
+      		cellH: 160,
+           gutterY: 8,
+           gutterX: 8,
+      		fixSize: 0,
+      		onResize: function() {
+      			wall.refresh();
+      		}
+      	});
+                                   wall.fitZone();
+                                   wall.filter(".na");
+      	$(".filter-label").click(function() {
+      		$(".filter-label").removeClass("active");
+      		var filter = $(this).addClass('active').data('filter');
+      		if (filter) {
+      			wall.filter(filter);
+      		} else {
+      			wall.unFilter();
+      		}
+      	});
+      
+      	wall.fitWidth();
+      });
+   </script>
+   <script type="text/javascript" src="https://static.boards.lol/js/ganalytics.js"></script>
+}
